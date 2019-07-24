e5a582b73ef6c69d9f1fbbba3e9145adfff335ca
EVA <seny_ussur@mail.ru>
2019-07-24 15:11:15 +1000

    Ветка первого решения

--- pages/locators.py
+++ pages/locators.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
-    LOGIN_LINK=(By.CSS_SELECTOR,"#registration_link_err")
-    LOGIN_FORM=(By.CSS_SELECTOR,"#login_form_err")
-    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form_err")
+    LOGIN_LINK=(By.CSS_SELECTOR,"#login_link")
+    LOGIN_FORM=(By.CSS_SELECTOR,"#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from .pages.locators import MainPageLocators
+from .locators import MainPageLocators
 
 
 class LoginPage(BasePage):
